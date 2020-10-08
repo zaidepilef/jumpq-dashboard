6157fe1b641d2e0517a56695f296c9543e86a5bc
Felipe Diaz <fel.di.rod@gmail.com>
2020-10-08 11:36:36 -0300

    con https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   API_EMAIL_URL: 'http://jumpq.cl/restapi/mailman/',
-  API_URL: 'http://jumpnodeapi.azurewebsites.net/api',
-  VERIFY_EMAIL_URL: 'http://jumpnodeapi.azurewebsites.net/verifyemail'
+  API_URL: 'https://jumpnodeapi.azurewebsites.net/api',
+  VERIFY_EMAIL_URL: 'https://jumpnodeapi.azurewebsites.net/verifyemail'
 };
 
